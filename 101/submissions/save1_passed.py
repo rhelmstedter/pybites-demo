1fc3bb807b2071c0d286de79ce757a341233558b
Russell Helmstedter <rhelmstedter@gmail.com>
2022-09-10 11:13:40 -0700

    bite 101

---
+++ 101/submissions/save1_passed.py
@@ -0,0 +1,10 @@
+MIN_DRIVING_AGE = 18
+
+
+def allowed_driving(name: str, age: int) -> None:
+    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+       checking the passed in age against the MIN_DRIVING_AGE constant"""
+    if age < 18:
+        print(f'{name} is not allowed to drive')
+    else:
+        print(f'{name} is allowed to drive')
